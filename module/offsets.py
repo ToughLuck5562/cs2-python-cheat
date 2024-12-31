38ceb900e95bbf0a913a5a4336ce50dd0aff9cdc
ToughLuck <94546656+ToughLuck5562@users.noreply.github.com>
2024-12-30 23:43:46 -0500

    Update offsets.py

--- module/offsets.py
+++ module/offsets.py
@@ -5,7 +5,7 @@ from requests import get
 class Offsets:
     try:
         offset = get("https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/offsets.json").json()
-        client = get("https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/client.dll.json").json()
+        client = get("https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/client_dll.json").json()
 
         dwEntityList = offset["client.dll"]["dwEntityList"]
         dwViewMatrix = offset["client.dll"]["dwViewMatrix"]
